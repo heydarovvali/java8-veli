418572e6fdbac4466cfb0704a2db02c6c433887d
heydarovvali <110536438+heydarovvali@users.noreply.github.com>
2023-10-25 03:30:05 +0400

    35 spring mvc project created

---
+++ spring/spring-mvc-example/spring-mvc-example/src/test/java/az/bakucode/springmvcexample/SpringMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package az.bakucode.springmvcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
