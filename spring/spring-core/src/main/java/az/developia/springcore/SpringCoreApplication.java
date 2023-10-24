4fe3ce19644f0cd39fd7370acc3141699e442a9b
heydarovvali <110536438+heydarovvali@users.noreply.github.com>
2023-10-24 20:46:03 +0400

    ev isi (Spring 34)

--- spring/spring-core/src/main/java/az/developia/springcore/SpringCoreApplication.java
+++ spring/spring-core/src/main/java/az/developia/springcore/SpringCoreApplication.java
@@ -8,15 +8,22 @@ import org.springframework.context.ApplicationContext;
 //config
 //component scan
 //auto config
+
 public class SpringCoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext c = SpringApplication.run(SpringCoreApplication.class, args);
-        Student s = c.getBean(Student.class);
-        s.name = "Ali";
-        System.out.println(s.name);
-        Student s2 = c.getBean(Student.class);
-        System.out.println(s2.name);
+//        Student s = c.getBean(Student.class);
+//        s.name = "Ali";
+//        System.out.println(s.name);
+//        Student s2 = c.getBean(Student.class);
+//        System.out.println(s2.name);
+        Person f = c.getBean(Person.class);
+        System.out.println(f.getId());
+        System.out.println(f.getName());
+        System.out.println(f.getAge());
+        System.out.println(f.getSalary());
+
 
     }
 
